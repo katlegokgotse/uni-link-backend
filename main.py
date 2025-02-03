51d081e1eacdcd587b79536079d8fca48fc4631a
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-02-03 21:14:35 +0200

    Main: fixing the publish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 import os
 
 # Initialize Flask app
